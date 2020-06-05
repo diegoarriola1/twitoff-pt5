c366a2444dc6ae905ebe01d396b28e136654f27c
Diego Arriola <diego@MacBook-Pro.local>
2020-06-05 00:19:55 -0400

    testing ssh key add

--- web_app/models.py
+++ web_app/models.py
@@ -39,3 +39,6 @@ def parse_records(database_records):
         del parsed_record["_sa_instance_state"]
         parsed_records.append(parsed_record)
     return parsed_records
+
+
+# testing git upload
